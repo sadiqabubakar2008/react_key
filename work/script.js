4bc252d0366c5b6e17b0f4720705323405c1adf8
sadiq Abubakar 2008 <sadiqabubakardanmusaa@gmail.com>
2026-02-17 13:35:54 +0100

    Edit text, & the use of the "enter" button to add task

--- work/script.js
+++ work/script.js
@@ -1,8 +1,26 @@
 let taskInput = document.getElementById("taskInput");
 let addBtn = document.getElementById("addBtn");
 let taskList = document.getElementById("taskList");
+let themeToggle = document.getElementById("themeToggle");
 
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  themeToggle.textContent = "☀️";
+}
+
+themeToggle.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    themeToggle.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    themeToggle.textContent = "🌙";
+  }
+});
 
 displayTasks();
 
@@ -50,8 +68,27 @@ function displayTasks() {
       span.classList.add("completed");
     }
 
+    let editBtn = document.createElement("button");
+    editBtn.innerHTML = "✏️";
+
+    editBtn.addEventListener("click", function () {
+
+      let editInput = document.createElement("input");
+      editInput.type = "text";
+      editInput.value = tasks[i].text;
+
+      li.replaceChild(editInput, span);
+      editBtn.innerHTML = "💾";
+
+      editBtn.onclick = function () {
+        tasks[i].text = editInput.value;
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        displayTasks();
+      };
+    });
+
     let deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "x";
+    deleteBtn.textContent = "X";
 
     deleteBtn.addEventListener("click", function () {
       tasks.splice(i, 1);
@@ -61,27 +98,15 @@ function displayTasks() {
 
     li.appendChild(checkbox);
     li.appendChild(span);
+    li.appendChild(editBtn);
     li.appendChild(deleteBtn);
 
     taskList.appendChild(li);
   }
 }
 
-
-let themeToggle = document.getElementById("themeToggle");
-
-if (localStorage.getItem("theme") === "dark") {
-  document.body.classList.add("dark");
-}
-
-themeToggle.addEventListener("click", function () {
-  document.body.classList.toggle("dark");
-
-  if (document.body.classList.contains("dark")) {
-    localStorage.setItem("theme", "dark");
-    themeToggle.textContent = "☀️";
-  } else {
-    localStorage.setItem("theme", "light");
-    themeToggle.textContent = "🌙";
+taskInput.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    addTask();
   }
 });
